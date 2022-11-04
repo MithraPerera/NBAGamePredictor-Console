d303ec6fa614dfed7c78b6460f40f28863f58dba
Mithra Perera <mithraperera@hotmail.com>
2022-11-04 01:39:02 -0400

    csv reader package

---
+++ NbaGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace NBAGamePredictor
+{
+    public class NbaGame
+    {
+        public NbaGame()
+        {
+        }
+
+        public int matchnumber { get; set; }
+        public int roundnumber { get; set; }
+        public DateTime date { get; set; }
+        public string location { get; set; }
+        public string hometeam { get; set; }
+        public string awayteam { get; set; }
+        public string result { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using NBAGamePredictor;
 
 string csv_file_path = @"/Users/mithraperera/DEV/PROJECTS/NBA Game Predictor/NBAGamePredictor/nba-2021-UTC.csv";
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    PrepareHeaderForMatch = args => args.Header.ToLower().Trim(),
+};
+
+using (var reader = new StreamReader(csv_file_path))
+using (var csv = new CsvReader(reader, config))
+{
+    var records = csv.GetRecords<NbaGame>();
+    Console.WriteLine(records);
+}
